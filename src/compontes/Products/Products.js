52060b4a6ae251dc8ea9720d2d15e0b1484b1c31
SudiptaDeyAnik08 <sudiptadeysd06@gmail.com>
2023-02-15 23:59:10 +0600

    Simple React website

--- src/compontes/Products/Products.js
+++ src/compontes/Products/Products.js
@@ -1,14 +1,32 @@
 import React from "react";
-import './Products.css'
+import './Products.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee,faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+
 const Products=(props)=>{
+    // const {data,addToCardHandeler} = props;
+    // console.log(props);
     const {name, img, seller, price, ratings} = props.data;
+
+    // const {addToCardHandeler} = props.addToCardHandeler
+    
+    // console.log(addToCardHandeler)
+
     return(
         <div className="Product">
-            {
-                console.log(props)
-            }
             <img src={img} alt="" />
-            <p>{name}</p>
+           <div className="product-info">
+                <p className="Single_goods_name">{name}</p>
+                <p>Price: {price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings: {ratings}stars</small></p>
+           </div>
+           <button onClick= {()=>props.addToCardHandeler(props.data)} className="btn-cart"> <p>Add to Cart </p>
+           <FontAwesomeIcon icon={faShoppingCart} />
+           </button>
+          
         </div>
     )
 }
--- src/compontes/Shop/Shop.js
+++ src/compontes/Shop/Shop.js
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from "react";
 import './Shop.css'
 import Products from '../Products/Products.js'
 
+
+
 const Shop = ()=>{
 
     const [product,setProduct] = useState([]);
+    const [cart,setCart] = useState([]);
+   
 
     useEffect(()=>{
         fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
@@ -12,17 +16,22 @@ const Shop = ()=>{
         .then(data => setProduct(data))
     },[])
 
+    const addToCardHandeler = (respondGoods) =>{
+        console.log(respondGoods);
+        const newCart = [...cart,respondGoods];
+        setCart(newCart);
+    }
 
     return(
         <div className="Parent">
            <div className="Product-container" >
-           
            {
-            product.map(res => <Products key = {res.id} data ={res}></Products>)
+            product.map(res => <Products key = {res.id} data ={res} addToCardHandeler={addToCardHandeler}></Products>)
            }
            </div>
             <div>
                 <p>Oder Summary</p>
+                <p>Total : {cart.length}</p>
             </div>
         </div>
     )
